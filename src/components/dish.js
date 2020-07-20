37a918108fee8cad232b9a2b1df4fb0a29c01aa5
Jonathan Vargas <jonathanfelipe0111@hotmail.com>
2020-07-20 18:21:56 -0500

    "consumo de datos locales y remotos"
    This reverts commit 26046982c6f468ad6e2fcfef60adce71dae4b3cc.

--- src/components/dish.js
+++ src/components/dish.js
@@ -1,5 +1,4 @@
 import React, {Component , Fragment} from "react";
-import GridList from '@material-ui/core/GridList';
 
 
 //Clase del componente
@@ -23,21 +22,21 @@ class Ingredient extends Component{
     }
 }
 
-class Dish extends Component{ 
-    render(){        
+
+
+class Dish extends Component{
+    dishes = ["Tacos", "Ceviche","Paella"];
+    countIngredients(){
+        return this.dishes.length;
+    }
+    render(){
+        const { params} = this.props.match;
         return(
         <div className="dish">
-            <GridList cellHeight={160} cols={3}>
-                <div className="dish">
-                        <div>
-                            {this.props.name}
-                        </div>
-                        {this.props.ingredientes.map(
-                            (ingredientes, index) =>(
-                            <div key={index}>{ingredientes}</div>
-                            ))}
-                </div>
-            </GridList>
+            <h1>{params.name} </h1>
+            <h1>{this.props.name} </h1>
+            <h3>{this.countIngredients()}</h3>
+            <Ingredient/>            
         </div>
         )        
     }
